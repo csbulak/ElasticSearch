086b47af2ef4b5c5c335e7a9fc9ecc71a30c8d92
csbulak <csbulak@gmail.com>
2024-03-05 23:06:54 +0300

    Add Elasticsearch extension and configuration
    Introduced a new extension for Elasticsearch to set up and provide an Elasticsearch client as a singleton. Also, updated the Program.cs file to use this extension. The Elasticsearch server details are configured in the appsettings.Development.json file.

--- ElasticSearch.API/Program.cs
+++ ElasticSearch.API/Program.cs
@@ -1,13 +1,17 @@
+using ElasticSearch.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddElastic(builder.Configuration);
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
