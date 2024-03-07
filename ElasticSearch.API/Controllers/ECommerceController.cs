828ccbef017d5ec21c586f738488e2110ae4d33f
csbulak <csbulak@gmail.com>
2024-03-07 15:31:03 +0300

    Add ECommerce functionality in ElasticSearch API
    New classes for ECommerce have been created - ECommerceRepository in the Repository folder and ECommerceController in the Controllers folder. An ECommerce model has also been added to handle specific data structures. These additions aim to provide new query capabilities to search terms based on customer names in the ecommerce context.

---
+++ ElasticSearch.API/Controllers/ECommerceController.cs
@@ -0,0 +1,31 @@
+using ElasticSearch.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticSearch.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ECommerceController : ControllerBase
+    {
+        private readonly ECommerceRepository _eCommerceRepository;
+
+        public ECommerceController(ECommerceRepository eCommerceRepository)
+        {
+            _eCommerceRepository = eCommerceRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TermQuery(string customerFirstName)
+        {
+            var result = await _eCommerceRepository.TermQuery(customerFirstName);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TermsQuery(List<string> customerFirstName)
+        {
+            var result = await _eCommerceRepository.TermsQuery(customerFirstName);
+            return Ok(result);
+        }
+    }
+}
